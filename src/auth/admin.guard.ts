412beeb61a126daa8bb4f2058da55c426e50c2e9
ryan aragao <ryanaragao012@gmail.com>
2023-06-22 12:47:23 -0300

    register guard

--- src/auth/admin.guard.ts
+++ src/auth/admin.guard.ts
@@ -4,14 +4,32 @@ import {
   ExecutionContext,
   UnauthorizedException,
 } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AdminGuard implements CanActivate {
-  canActivate(context: ExecutionContext): boolean {
+  constructor(private jwtService: JwtService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
 
     if (!request.body.role || request.body.role === 'STUDENT') return true;
 
-    throw new UnauthorizedException();
+    const authToken = request.headers['authorization'];
+
+    if (!authToken) throw new UnauthorizedException();
+
+    const extractedToken = authToken.split(' ')[1];
+
+    try {
+      await this.jwtService.verifyAsync(extractedToken, {
+        secret: process.env.SECRET_KEY,
+      });
+    } catch (error) {
+      console.log('test');
+      throw new UnauthorizedException();
+    }
+
+    return true;
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,17 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AdminGuard } from './admin.guard';
+import { JwtService, JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [],
-  providers: [AuthService, PrismaService, AdminGuard],
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: process.env.SECRET_KEY,
+      signOptions: { expiresIn: '30d' },
+    }),
+  ],
+  providers: [AuthService, PrismaService, AdminGuard, JwtService],
   controllers: [AuthController],
 })
 export class AuthModule {}
